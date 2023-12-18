77e80bc0609c980a82d7519bfb8d65c8c0d2250a
Leonardo Valle <leo.mvm@hotmail.com>
2023-12-18 13:19:45 -0300

    fix drawer not fully closing issues

--- componentlibrary/src/main/java/com/vallem/componentlibrary/ui/drawer/SylphNavigationDrawer.kt
+++ componentlibrary/src/main/java/com/vallem/componentlibrary/ui/drawer/SylphNavigationDrawer.kt
@@ -1,17 +1,14 @@
 package com.vallem.componentlibrary.ui.drawer
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -22,7 +19,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vallem.componentlibrary.domain.model.UserInfo
@@ -33,7 +29,6 @@ import com.vallem.componentlibrary.ui.theme.ZoneEventColors
 import com.vallem.componentlibrary.ui.user.UserInfo
 
 object SylphNavigationDrawer {
-    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     operator fun <T : SylphShortcut> invoke(
         userInfo: UserInfo,
@@ -93,28 +88,25 @@ object SylphNavigationDrawer {
         middleContent: @Composable ColumnScope.() -> Unit,
         bottomContent: @Composable ColumnScope.() -> Unit,
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = modifier
-                .clip(RoundedCornerShape(topEnd = 16.dp, bottomEnd = 16.dp))
-                .background(MaterialTheme.colorScheme.surface)
-                .padding(vertical = 24.dp)
-                .fillMaxWidth(0.85f)
-                .fillMaxHeight(),
-        ) {
-            topContent()
-
-            Divider(modifier = Modifier.fillMaxWidth())
-
+        ModalDrawerSheet(modifier = modifier.fillMaxWidth(.85f)) {
             Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(vertical = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(vertical = 24.dp),
             ) {
-                middleContent()
-            }
+                topContent()
 
-            bottomContent()
+                Divider(modifier = Modifier.fillMaxWidth())
+
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(vertical = 16.dp),
+                ) {
+                    middleContent()
+                }
+
+                bottomContent()
+            }
         }
     }
 }
